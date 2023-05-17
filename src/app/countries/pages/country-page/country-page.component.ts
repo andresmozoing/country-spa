88285b3498f1a34fde7ccf33ec41da3b8d5242ca
Andres <andresmozoing@gmail.com>
2023-05-17 16:38:43 -0300

    Se agrega la busqueda y la tabla en las otras dos pantallas Se arranca la pantalla de info especifica de pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Params } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+
+  constructor (private ActivatedRoute : ActivatedRoute,
+              private CountriesService : CountriesService ) {}
+
+  ngOnInit(): void {
+    this.ActivatedRoute.params
+      .subscribe( params => {
+        console.log("params es " + params['id']);
+        const id = params['id']
+        this.CountriesService.searchCountryByAlphaCode(id)
+          .subscribe(country =>{
+            console.log("country es " , country);
+
+          })
+      })
+  }
+
 
 }
